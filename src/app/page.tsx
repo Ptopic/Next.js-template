61c7684d0fbc339e03b1536fe72f89bba5eb5131
Petar <petartopic2002@gmail.com>
2025-03-16 12:11:18 +0100

    Fix prettier not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,57 +6,57 @@ import { openGraphImage } from 'metadata/openGraphImage';
 import { Metadata, ResolvingMetadata } from 'next';
 
 export async function generateMetadata(
-	{},
-	parent: ResolvingMetadata
+   {},
+   parent: ResolvingMetadata
 ): Promise<Metadata> {
-	const parentOpengraphUrl = (await parent).openGraph?.url;
+   const parentOpengraphUrl = (await parent).openGraph?.url;
 
-	return {
-		title: getMetadataTitle('Home'),
-		openGraph: {
-			title: getMetadataTitle('Home'),
-			url: `${parentOpengraphUrl}${EARNING}`,
-			...openGraphImage,
-		},
-	};
+   return {
+      title: getMetadataTitle('Home'),
+      openGraph: {
+         title: getMetadataTitle('Home'),
+         url: `${parentOpengraphUrl}${EARNING}`,
+         ...openGraphImage,
+      },
+   };
 }
 
 const Home = async () => {
-	const queryClient = getSSRQueryClient();
+   const queryClient = getSSRQueryClient();
 
-	// await Promise.all([
-	//   queryClient.prefetchQuery({
-	//     queryKey: instancesQueryKey,
-	//     queryFn: () =>
-	//       instancesServerRequests.getInstances({
-	//         page,
-	//         pageSize,
-	//         search,
-	//         gpu,
-	//         region,
-	//         sortField: sortField,
-	//         sortDirection: sortDirection,
-	//       }),
-	//   }),
-	//   queryClient.prefetchQuery({
-	//     queryKey: [GPU_NAMES],
-	//     queryFn: () => instancesServerRequests.getMachinesGpuName(),
-	//   }),
-	//   queryClient.prefetchQuery({
-	//     queryKey: [CURRENT_USER],
-	//     queryFn: () => authServerRequests.authCurrentUser(),
-	//   }),
-	// ]);
+   // await Promise.all([
+   //   queryClient.prefetchQuery({
+   //     queryKey: instancesQueryKey,
+   //     queryFn: () =>
+   //       instancesServerRequests.getInstances({
+   //         page,
+   //         pageSize,
+   //         search,
+   //         gpu,
+   //         region,
+   //         sortField: sortField,
+   //         sortDirection: sortDirection,
+   //       }),
+   //   }),
+   //   queryClient.prefetchQuery({
+   //     queryKey: [GPU_NAMES],
+   //     queryFn: () => instancesServerRequests.getMachinesGpuName(),
+   //   }),
+   //   queryClient.prefetchQuery({
+   //     queryKey: [CURRENT_USER],
+   //     queryFn: () => authServerRequests.authCurrentUser(),
+   //   }),
+   // ]);
 
-	const dehydratedState = dehydrate(queryClient);
+   const dehydratedState = dehydrate(queryClient);
 
-	const cookie = await getCookie(COOKIE_NAME.ACCESS_TOKEN);
-	console.log(cookie);
-	return (
-		<HydrationBoundary state={dehydratedState}>
-			<h1>HOME</h1>
-		</HydrationBoundary>
-	);
+   const cookie = await getCookie(COOKIE_NAME.ACCESS_TOKEN);
+   console.log(cookie);
+   return (
+      <HydrationBoundary state={dehydratedState}>
+         <h1>HOME</h1>
+      </HydrationBoundary>
+   );
 };
 
 export default Home;
